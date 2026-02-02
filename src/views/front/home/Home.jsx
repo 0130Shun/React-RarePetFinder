c4672e4a2dceea481f088b2a07d87728ec01d4d0
UN <youen1126@gmail.com>
2026-02-02 15:06:12 +0800

    fix:main.scss和Home.jsx匯入Scam.jsx、RepostPost.jsx以及對應的scss

--- src/views/front/home/Home.jsx
+++ src/views/front/home/Home.jsx
@@ -1,7 +1,12 @@
-import Tranding from "./sections/Tranding"
+import Tranding from "./sections/Tranding";
+import Scam from "./sections/Scam";
+import RepostPost from "./sections/RepostPost";
+
 export default function Home() {
     return (<>
         <h1>這是首頁</h1>
         <Tranding />
+        <Scam />
+        <RepostPost />
     </>)
 }
