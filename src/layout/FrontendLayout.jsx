9cd6e6532471c3774794a065941c2fe688d89bd4
Rudy <s60990340@gmail.com>
2026-02-02 15:31:33 +0800

    安裝 bootstrap; 首頁 heroSection 、Searchbar 上傳完成; 新增有用到的圖片 提問 FrontendLayout.jsx 中的 className 可否刪除?

--- src/layout/FrontendLayout.jsx
+++ src/layout/FrontendLayout.jsx
@@ -1,18 +1,19 @@
 //主控layout 放header、footer、main
 
-import { Outlet, NavLink } from 'react-router-dom';
+import { Outlet, NavLink } from "react-router-dom";
 
-import Header from '../components/Header'; // 可選
+import Header from "../components/Header"; // 可選
 
 export default function FrontendLayout() {
   return (
     <>
       <Header />
-      <main class='container ui-layout'>
+      <main class="container ui-layout">
+        {/* 請問這個 container 是可以拿掉的嗎? */}
         <Outlet />
       </main>
       <hr />
-      <footer className='mt-4 text-center'>
+      <footer className="mt-4 text-center">
         <p>footer</p>
       </footer>
     </>
--- src/views/front/home/Home.jsx
+++ src/views/front/home/Home.jsx
@@ -1,8 +1,10 @@
+import HeroSection from "./sections/HeroSection";
 import HotSpot from "./sections/HotSpot";
 
 export default function Home() {
   return (
     <>
+      <HeroSection />
       <h1>這是首頁</h1>
       <HotSpot />
     </>
