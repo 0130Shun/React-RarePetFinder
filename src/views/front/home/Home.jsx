47aa2ac1c453f41010dca4c9082ec7892d627c4e
Rudy <s60990340@gmail.com>
2026-02-02 14:52:33 +0800

    安裝 React 中的 Swiper, feather 套件

--- src/views/front/home/Home.jsx
+++ src/views/front/home/Home.jsx
@@ -1,6 +1,10 @@
+import HotSpot from "./sections/HotSpot";
 
 export default function Home() {
-    return (<>
-        <h1>這是首頁</h1>
-    </>)
+  return (
+    <>
+      <h1>這是首頁</h1>
+      <HotSpot />
+    </>
+  );
 }
