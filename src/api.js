c1da4c882fdbe0e287d71a6dcfac8b705f7b8255
nonwhen07 <30990876+nonwhen07@users.noreply.github.com>
2026-02-27 02:50:59 +0800

    feat: 專欄+詐騙資訊api準備

--- src/api.js
+++ src/api.js
@@ -1,6 +1,10 @@
 import axios from 'axios';
 // const API_URL = 'http://localhost:3001';
 // 請建立.env後，請設定"VITE_API_URL=http://localhost:3001"
+// 如果沒全域安裝，請加入 npx：npx json-server -w db.json -p 3001
+// 啟動指令： json-server --watch db.json --port 3001 或簡寫： json-server -w db.json -p 3001
+// auth動指令：json-server-auth db.json --port 3001 簡寫： json-server-auth -p 3001 db.json
+
 const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3001';
 
 const apiRequest = axios.create({
@@ -14,7 +18,6 @@ export const storeService = {
     const res = await apiRequest.get('/stores');
     return Array.isArray(res.data) ? res.data : [];
   },
-
   // 單一店家
   async getStoreDetail(id) {
     const res = await apiRequest.get(`/stores/${id}`);
@@ -25,5 +28,21 @@ export const storeService = {
   async getAllArticles() {
     const respones = await apiRequest.get('/articles');
     return Array.isArray(respones.data) ? respones.data : [];
+  },
+  // 單一專欄
+  async getArticle(id) {
+    const res = await apiRequest.get(`/articles/${id}`);
+    return res.data;
+  },
+
+  //詐騙資訊列表
+  async getAllAnnouncements() {
+    const respones = await apiRequest.get('/announcements');
+    return Array.isArray(respones.data) ? respones.data : [];
+  },
+  // 單一詐騙資訊
+  async getAnnouncement(id) {
+    const res = await apiRequest.get(`/announcements/${id}`);
+    return res.data;
   },
 };
