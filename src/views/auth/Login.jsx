a49987b3e791f949b9e5f1e6c8900df0f10c26d9
nonwhen07 <30990876+nonwhen07@users.noreply.github.com>
2026-03-14 05:35:18 +0800

    fix: handleLogin相關

--- src/views/auth/Login.jsx
+++ src/views/auth/Login.jsx
@@ -1,14 +1,12 @@
 import { useState } from 'react';
-// import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 import FullPageLoader from '@/components/shared/FullPageLoader';
 
-// import { login } from '@/services/authService';
-// import { handleApiError } from '@/utils/apiErrorHandler';
-// import { loginSuccess } from '@/features/auth/authSlice';
+import { handleApiError } from '@/utils/apiErrorHandler';
 
 import { useDispatch } from 'react-redux';
-// import { useToast } from '@/hooks/useToast';
+import { useToast } from '@/hooks/useToast';
 
 // Service
 import { loginApi } from '@/services/authService';
@@ -20,85 +18,58 @@ const LoginPage = () => {
   // 初始化 dispatch
   const dispatch = useDispatch();
   // // 初始化 navigate
-  // const navigate = useNavigate();
-  // // const { success, showError, warning } = useToast();
+  const navigate = useNavigate();
+  const { success, showError, warning } = useToast();
 
   const [account, setAccount] = useState({
     username: 'example@test.com',
     password: 'example',
   });
-  // const [errorMessage, setErrorMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
   const [isScreenLoading, setIsScreenLoading] = useState(false);
 
   // // 登入表單 - 登入submit事件（使用 async/await）
   const handleLogin = async (e) => {
     e.preventDefault(); // 一定要最前面，避免後續程式碼執行後頁面刷新
-    // return; // 在其他功能做好前都先return，僅先有btn
     setIsScreenLoading(true);
-    // setErrorMessage('');
-    // if (!account.username || !account.password) {
-    //   setErrorMessage('請填寫完整登入資訊');
-    //   warning('請填寫完整登入資訊');
-    //   setIsScreenLoading(false);
-    //   return;
-    // }
-
-    // try {
-    //   const res = await login(account);
-    //   const { token, expired } = res;
-    //   document.cookie = `hexToken_week7=${token}; path=/; expires=${new Date(
-    //     expired
-    //   ).toUTCString()}`;
-
-    //   success('登入成功，將導向後台首頁');
-    //   dispatch(
-    //     loginSuccess({
-    //       token,
-    //       user: res.user || null,
-    //     })
-    //   );
-    //   // 延遲導航 0.5 秒，UIUX更好一點點
-    //   setTimeout(() => {
-    //     navigate('/admin');
-    //   }, 500);
-    // } catch (err) {
-    //   // 如果其他page沒有const [errorMessage, setErrorMessage] = useState('');，
-    //   // 去顯示錯誤訊息或其他資訊可以改填 handleApiError(error, undefined, '登出失敗');
-    //   // 或 handleApiError(error, null);
-    //   // ex： const message = handleApiError(error, null, '登出失敗，請重新嘗試。');
-    //   const errorMessage = handleApiError(
-    //     err,
-    //     setErrorMessage,
-    //     '登出失敗，請重新嘗試。'
-    //   );
-
-    //   showError(errorMessage);
-    // } finally {
-    //   setIsScreenLoading(false);
-    // }
+    setErrorMessage(''); // 清空錯誤資訊區
 
     if (!account.username || !account.password) {
-      // setErrorMessage('請填寫完整登入資訊');
-      // warning('請填寫完整登入資訊');
+      setErrorMessage('請填寫完整登入資訊');
+      warning('請填寫完整登入資訊');
       setIsScreenLoading(false);
-      console.warn('請填寫完整登入資訊');
       return;
     }
 
     try {
       const res = await loginApi(account);
-      localStorage.setItem('token', res.accessToken);
-      const token = localStorage.getItem('token');
-      const user = res.user;
+      const { accessToken, expired, user } = res;
 
-      if (token) {
-        dispatch(setToken({ token }));
-        dispatch(setUser(user));
-        console.log('登入成功：token', token);
-        console.log('登入成功：user', user);
-      }
-    } catch (error) {
-      console.log(error);
+      const token = accessToken;
+      document.cookie = `RarePetFinder=${token}; path=/; expires=${new Date(
+        expired
+      ).toUTCString()}`;
+
+      localStorage.setItem('token', token);
+
+      dispatch(setToken({ token }));
+      dispatch(setUser(user));
+
+      console.log('登入成功', { token, user }); // testconsole
+
+      success('登入成功，將導向後台首頁');
+
+      setTimeout(() => {
+        //由於現在沒有後台，先假裝home真的跳躍了
+        navigate('/');
+      }, 500);
+    } catch (err) {
+      const errorMessage = handleApiError(
+        err,
+        setErrorMessage,
+        '登出失敗，請重新嘗試。'
+      );
+      showError(errorMessage);
     } finally {
       setIsScreenLoading(false);
     }
@@ -117,9 +88,9 @@ const LoginPage = () => {
     <>
       <div className="d-flex flex-column justify-content-center align-items-center vh-100">
         <h1 className="mb-5">請先登入</h1>
-        {/* {errorMessage && (
+        {errorMessage && (
           <div className="alert alert-danger w-100">{errorMessage}</div>
-        )} */}
+        )}
         <form onSubmit={handleLogin} className="d-flex flex-column gap-3">
           <div className="form-floating mb-3">
             <input
