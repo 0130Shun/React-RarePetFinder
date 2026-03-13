4bfada42e4eaa6ce3f9973b1f6dd27abfe724170
nonwhen07 <30990876+nonwhen07@users.noreply.github.com>
2026-03-14 04:49:16 +0800

    feat: 帶入toastSlice相關

--- src/store/store.js
+++ src/store/store.js
@@ -2,13 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 
 import authReducer from '@/features/authSlice';
 // 由於沒有回傳會員名稱和權限狀態的API，userSlice先製作後暫時不使用
-import userReducer from '@/features/user/userSlice';
-// import toastReducer from '@/features/toast/toastSlice';
+import userReducer from '@/features/userSlice';
+import toastReducer from '@/features/toastSlice';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     user: userReducer,
-    // toast: toastReducer,
+    toast: toastReducer,
   },
 });
